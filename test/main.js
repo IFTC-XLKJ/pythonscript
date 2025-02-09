bc12394ef4d94e7feb6daaf28364e0d972544825
IFTC-XLKJ <iftcceo@139.com>
2025-02-09 12:07:19 +0800

    Update main.js

--- test/main.js
+++ test/main.js
@@ -1,4 +1,24 @@
 const textarea = document.getElementById('textarea');
+const result = document.getElementById('result');
 textarea.value = 'print("Hello World!")';
+pys.init();
+let isReady = false;
+pys.on('ready', ()=>{
+    isReady = true;
+})
 textarea.addEventListener('input', () => {
 });
+textarea.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+    pys.reset()
+    if (!isReady) return;
+    init();
+    pys.run(textarea.value);
+})
+function init() {
+    pys.on("start", e => {
+        result.innerHTML = ""
+        console.log(e);
+        result.innerHTML += "开始运行Python"
+    })
+}
