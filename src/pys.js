5a2252767561ac8ab61a7c5dfd7dedeec549427a
IFTC-XLKJ <iftcceo@139.com>
2025-02-09 11:50:38 +0800

    Update pys.js

--- src/pys.js
+++ src/pys.js
@@ -1,14 +1,34 @@
-globalThis.pys = {}
-pys.__proto__.run = function() {
-}
-pys.#events = {}
-pys.__proto__.on = function(event, callback) {
-    if (pys.#events[event] == undefined) {
-        pys.#events[event] = []
-    }
-    pys.#events[event].push(callback)
-}
-pys.#emit = function(event, data) {
-}
+const pys = (function () {
+    const privateData = {
+        events: {},
+        /**
+         * 触发事件
+         * @param {String} event 
+         * @param {Object} data 
+         */
+        emit: function (event, data) {
+            if (privateData.events[event]) {
+                privateData.events[event].forEach(callback => callback(data));
+            }
+        }
+    };
 
+    return {
+        run: function () {
+        },
+        /**
+         * 监听事件
+         * @param {String} event 
+         * @param {Function} callback 
+         */
+        on: function (event, callback) {
+            if (privateData.events[event] === undefined) {
+                privateData.events[event] = [];
+            }
+            privateData.events[event].push(callback);
+        },
+    };
+})();
+
+globalThis.pys = pys;
 pys.this = pys
