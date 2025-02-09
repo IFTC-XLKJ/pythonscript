51aa1b9e1f005fc7d8cafa8a42bbcf4992b13e99
IFTC-XLKJ <iftcceo@139.com>
2025-02-09 11:43:55 +0800

    Update pys.js

--- src/pys.js
+++ src/pys.js
@@ -1,5 +1,14 @@
 globalThis.pys = {}
 pys.__proto__.run = function() {
 }
+pys.#events = {}
+pys.__proto__.on = function(event, callback) {
+    if (pys.#events[event] == undefined) {
+        pys.#events[event] = []
+    }
+    pys.#events[event].push(callback)
+}
+pys.#emit = function(event, data) {
+}
 
 pys.this = pys
